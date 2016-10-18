153154c8431d84fa257584a9e1562196ae106d25
mburakergenc <muhammetergenc@gmail.com>
2016-10-17 22:10:22 -0500

    Open ports are outputted. Small bugs are fixed.

--- Assignment1.py
+++ Assignment1.py
@@ -6,21 +6,21 @@ filedir = raw_input("Enter the folder name: ")
 filename = raw_input("Enter the file name: ")
 
 try:
-    original_umask = os.umask(0)
     os.mkdir(filedir, 0777)
-    os.umask(original_umask)
 except OSError:
     pass
 while os.path.exists("%s/%s" % (filedir, filename)):
-    selection = raw_input("The file already exists. Do you want to override it? y-n")
-    if selection == 'y':
+    selection = raw_input("The file already exists. Do you want to override it?: y-n: ")
+    if selection.lower() == 'y': # Could use .casefold() in python 3
         os.remove("%s/%s" % (filedir, filename))
         break
     else:
         filename = raw_input("Enter the file name: ")
 
 Nscanner = nmap.PortScanner()
-Nscanner.scan(hosts=host, arguments="-open -n -T4 -oN %s/nmapformat.txt" % filedir)
+Nscanner.scan(hosts=host, arguments="-open -Pn -p 1-65535 -T4 -oN %s/scanresults.nmap" % filedir)
+print ('------------------ Open Ports ------------------------------')
+os.system("cat %s/scanresults.nmap | grep tcp | cut -d'/' -f1 | sort | uniq" % filedir)
 # create the list for the live IPs
 # the value x is echange for the value in the list in the library in all_hosts
 # pentes.all_hosts() is a function to list all the hosts scanned
